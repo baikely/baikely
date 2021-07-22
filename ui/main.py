75b97b22a7249231657b308690db59c4b1339e87
Anthony Li <antli@seas.upenn.edu>
2021-07-22 13:19:07 -0400

    Make window smaller

--- ui/main.py
+++ ui/main.py
@@ -4,21 +4,22 @@ def run():
     # Set up
     clock = pygame.time.Clock()
     pygame.init()
+    pygame.display.set_caption("Baike")
     pygame.font.init()
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((480, 320))
 
     # Set background
     screen.fill((255, 255, 255))
 
     # Draw bike
     bike = pygame.image.load("ui/bike.png")
-    bike = pygame.transform.scale(bike, (125, 125))
-    screen.blit(bike, (330, 150))
+    bike = pygame.transform.scale(bike, (75, 75))
+    screen.blit(bike, (200, 100))
 
     # Draw car
     car = pygame.image.load("ui/car.png")
-    car = pygame.transform.scale(car, (50, 50))
-    screen.blit(car, (370, 400))
+    car = pygame.transform.scale(car, (25, 25))
+    screen.blit(car, (225, 200))
 
     # Main loop
     running = True
