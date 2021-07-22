106a8a69d5e393f302bc5823fe836350cc2dba63
Anthony Li <antli@seas.upenn.edu>
2021-07-22 12:39:42 -0400

    Add car image

--- ui/main.py
+++ ui/main.py
@@ -7,12 +7,19 @@ def run():
     pygame.font.init()
     screen = pygame.display.set_mode((800, 600))
 
+    # Set background
+    screen.fill((255, 255, 255))
+
     # Draw text
     font = pygame.font.SysFont("Helvetica", 24)
-    text_surface = font.render("HERE'S A WINDOW", False, (255, 255, 255))
+    text_surface = font.render("HERE'S A WINDOW", False, (0, 0, 0))
     screen.blit(text_surface, (100, 100))
     text_surface = font.render("I FEEL TIRED", False, (0, 255, 255))
     screen.blit(text_surface, (100, 300))
+
+    car = pygame.image.load("ui/car.png")
+    car = pygame.transform.scale(car, (50, 50))
+    screen.blit(car, (100, 124))
 
     # Main loop
     running = True
