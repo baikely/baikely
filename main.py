03b230e94266993567247a5ba56e0204e317af7d
Anthony Li <antli@seas.upenn.edu>
2021-07-23 18:18:04 -0400

    Use PiGPIO

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ from multiprocessing import Process, Queue, Event
 from ui.main import run as run_ui
 from testprocess.main import run as run_testprocess
 from ui.ultrasonic import UltrasonicSensor
-from gpiozero.exc import BadPinFactory
+from gpiozero import Device
+from gpiozero.pins.pigpio import PiGPIOFactory
+import sys
 
 def main():
     try:
+        Device.pin_factory = PiGPIOFactory()
         sensors = [
-            UltrasonicSensor(echo=20, trigger=21, max_distance=4, start_angle=240, end_angle=300)
+            UltrasonicSensor(echo=20, trigger=21, max_distance=5, start_angle=240, end_angle=300)
         ]
-    except BadPinFactory:
-        print("Couldn't load a default pin factory. You're probably not running this on a Pi.")
+    except:
+        print("Error:", sys.exc_info()[0])
+        print("Couldn't initialize ultrasonic sensors. You're probably not running this on a Pi.")
         sensors = []
-
 
     queue = Queue()
     exit_event = Event()
-
-    testprocess = Process(target=run_testprocess, args=(queue, exit_event))
-    testprocess.start()
 
     run_ui(queue, sensors)
     exit_event.set()
