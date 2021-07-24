ef26116f58e56d13dbd083c1ca317b0b7d4ab61e
Anthony Li <antli@seas.upenn.edu>
2021-07-23 22:01:47 -0400

    Delete testprocess

--- testprocess/main.py
+++
@@ -1,8 +0,0 @@
-from multiprocessing import Queue, Event
-from time import sleep
-
-def run(queue: Queue, exit_event: Event):
-    print("Started test process.")
-    while not exit_event.is_set():
-        queue.put({"type": "test", "test": "Approved™"})
-        sleep(10)
